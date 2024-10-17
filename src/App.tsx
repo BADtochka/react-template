954b49dec4145530dccafb4ee514b2cf8f96fb8b
BADtochka <badmodertwitch@gmail.com>
2024-10-17 18:43:20 +0300

    feat: init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import '@/assets/styles/global.scss'
+import React from 'react'
+
+export const App = () => {
+  return (
+    <React.StrictMode>
+      <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh'}}>
+        <h1>hello world</h1>
+      </div>
+    </React.StrictMode>
+  )
+}
