478079109e37dccd1eb13b7c27aa1f9828c3280a
BADtochka <badmodertwitch@gmail.com>
2024-10-17 18:52:48 +0300

    feat: define env

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ export const App = () => {
   return (
     <React.StrictMode>
       <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh'}}>
-        <h1>hello world</h1>
+        <h1>hello world from {currentEnv}</h1>
       </div>
     </React.StrictMode>
   )
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="vite/client" />
+
+declare const isDev: boolean
+declare const isProd: boolean
+
+declare const currentEnv: string
