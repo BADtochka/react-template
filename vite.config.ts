60b08c74a87eaf313617ed4d0e1b5f3d9e3a7540
BADtochka <badmodertwitch@gmail.com>
2024-10-17 18:52:09 +0300

    fix: deprecate warnings

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,27 @@ import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({mode}) => ({
   plugins: [react()],
+  define: {
+    isDev: mode === 'development',
+    isProd: mode === 'production',
+    currentEnv: JSON.stringify(mode)
+  },
   resolve: {
     alias: {
       '@': '/src'
     }
-  }
-})
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "@/assets/styles/variables.scss";
+          @use "@/assets/styles/helpers.scss";
+        `,
+        silenceDeprecations: ['legacy-js-api'],
+      },
+    },
+  },
+}))
